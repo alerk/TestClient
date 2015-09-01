7ae32025f414ee4e8f44bdf3b9f7d9cf344678d7
Nguyen Xuan Quyen <quyen@debian>
2015-09-02 00:54:49 +0700

    [Alex]New version of TestClient, display time

--- src/TestClient.cpp
+++ src/TestClient.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
+#include <ctime>
 
 #include "Socket/ClientSocket.h"
 #include "Socket/SocketException.h"
@@ -16,28 +17,37 @@
 using namespace std;
 
 int main() {
+	time_t rawtime;
+	struct tm * timeinfo;
+	char buffer [80];
 	char* recv_buf;
+
+
 	recv_buf = (char*)malloc(15);
-//	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	//	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	int counter = 0;
 	while(true)
 	{
 		try
 		{
-//			system("clear");
+
+			//			system("clear");
 			cout << "\r" << std::flush;
 			cout << "Trying to connect " << counter++ << endl;
 			counter %= 10;
-			ClientSocket clientSock("192.168.100.103",13579);
+			ClientSocket clientSock("localhost",13579);
 			cout << "Open connection" << endl;
 			while(true)
 			{
+				time (&rawtime);
+				timeinfo = localtime (&rawtime);
+				strftime (buffer,80,"[%D - %H:%M:%S]",timeinfo);
 				int recv_size = clientSock.recv(recv_buf, 5);
 				if(recv_size<=0)
 				{
 					throw SocketException ( "Could not read from socket." );
 				}
-				printf("Receiving %d bytes from server:", recv_size);
+				printf("%sReceiving %d bytes from server:", buffer, recv_size);
 				for(int i=0;i<5;i++)
 				{
 					printf("%2.2x",(unsigned char)recv_buf[i]);
